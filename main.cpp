32e8cf229d559d6569dcd134c3444f0f59bee5d7
Nicholas 'Indy' Ray <arelius@gmail.com>
2011-11-30 01:16:08 -0800

    Added Span and Transform structures.
    * Basic data structure work to handle transformation of the password list, thus parsing and adding passwords.

--- main.cpp
+++ main.cpp
@@ -26,3 +26,63 @@ struct SecureBuffer
 
     // Don't trust realloc!! It's likely that it may leave the old data around.
 };
+
+typedef char* uchar; // Make this utf-8 or w/e.
+
+struct Span
+{
+    int offset;
+    int length;
+
+    Span* parent;
+    Span* sibling; // Next sibling in the list.
+    Span* children; // First child in the list.
+
+    Span() : offset(0),
+             length(0),
+             parent(null),
+             sibling(null),
+             children(null)
+    {}
+};
+
+// Document
+// +--Table
+//    +--Header
+//    +--Row
+//       +--Seperator
+//       +--Site
+//       +--User
+//       +--Password
+
+// Transforms can actually be really simple, if it's just a sorted list, we can
+// just adjust the following transforms offsets if we are doing in-memory, but
+// most likely we are transforming into a new buffer.
+
+struct Transform
+{
+    int offset;
+    int source_length;
+    int transform_length;
+
+    //API: Decide how we are going to manage this, do we copy,
+    //uchar* transform;
+
+    Transform* next;
+
+    Transform() : offset(0),
+                  source_length(0),
+                  transform_length(0),
+                  next(null)
+    {}
+
+    int lengthDelta()
+    {
+        return atomDelta() + (next ? next.lengthDelta() : 0);
+    }
+
+    int atomDelta()
+    {
+        return transform_length - source_length;
+    }
+};
